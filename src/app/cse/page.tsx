fe421efb12de5e3f00b1c29c3fa80d79b1a1cf94
Prakhar Bhandari <prakharb2k6@gmail.com>
2024-11-22 21:30:36 +0530

    fixed bugs in /cse

--- src/app/cse/page.tsx
+++ src/app/cse/page.tsx
@@ -1,27 +1,28 @@
 "use client";
 
+import React, { useState } from 'react';
 import DisciplineBox from '../components/disciplineBox';
 import Nav from '../components/nav';
 
 export default function Home() {
-  const items = ([
+  const [items] = useState([
     { text: "CS F111: Computer Programming", link: "/cse/csf111" },
     { text: "CS F222: Discrete Structures", link: "/cse/csf222" },
-    { text: "CS F211: Data Structures & Algos", link: "/cse/csf211" },
+    { text: "CS F211: Data Struc. & Algos", link: "/cse/csf211" },
   ]);
+
 
   return (
     <div>
-      <Nav text="ACIAX : The Academic Information Exchange Hub For BITS Goa" />
+      <Nav text="CSE : Computer Science Engineering" />
       <br />
-      <div style={{ position: "relative" }}>
+      <div style={{ position: "absolute", top:"85px" }}>
         {items.map((item, index) => (
           <DisciplineBox
             key={index}
-            // className="draggable"
             text={item.text}
             link={item.link}
-            style={{ position:'relative', display:'block' }}
+            style={{ position: 'relative', display:'block' }}
           />
         ))}
       </div>
