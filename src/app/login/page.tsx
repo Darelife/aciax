c3fcff61067489cb42bb80f0756da5c5a81bea04
Prakhar Bhandari <prakharb2k6@gmail.com>
2024-12-29 13:26:19 +0530

    added styling to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useContext, useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import AuthContext from '../../../context/AuthContext';
-import jwt from 'jsonwebtoken';
-import Cookies from 'js-cookie';
-import CryptoJs from 'crypto-js';
+import { useContext, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import AuthContext from "../../../context/AuthContext";
+import jwt from "jsonwebtoken";
+import Cookies from "js-cookie";
+import CryptoJs from "crypto-js";
 
 const Login = () => {
   const { login, logout, user, error } = useContext(AuthContext);
@@ -13,11 +13,9 @@ const Login = () => {
   const searchParams = useSearchParams();
   const [notValid, setNotValid] = useState(false);
   const [email, setEmail] = useState("");
-  // const thing = "bitCadAciaxFTW";
-  const thing:string = process.env.NEXT_PUBLIC_COOKIE_SECRET as string;
+  const thing: string = process.env.NEXT_PUBLIC_COOKIE_SECRET as string;
 
   useEffect(() => {
-    // const token = Cookies.get("token");
     if (user) {
       console.log("Email:", user?.email || "No email yet");
       setEmail(user.email);
@@ -26,7 +24,7 @@ const Login = () => {
       } else if (user.email) {
         const redirectTo = searchParams.get("redirectTo") || "/";
         router.push(redirectTo);
-        const token:string = jwt.sign({ email: user.email }, thing);
+        const token: string = jwt.sign({ email: user.email }, thing);
         const encryptedToken = CryptoJs.AES.encrypt(token, thing).toString();
         Cookies.set("auth", encryptedToken, { expires: 1 });
       }
@@ -40,20 +38,57 @@ const Login = () => {
   }, [notValid, logout]);
 
   return (
-    <div>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
       {notValid ? (
-        <div>
-          <h1>Invalid Email</h1>
+        <div style={{ textAlign: "center" }}>
+          <h1 style={{ color: "red" }}>Invalid Email</h1>
           <p>Please log in with your BITS Pilani Goa email.</p>
-          <button onClick={login}>Retry Login with Google</button>
-          <br />
-          {email}
+          <button
+            onClick={login}
+            style={{
+              marginTop: "10px",
+              padding: "15px 30px",
+              borderRadius: "5px",
+              border: "none",
+              backgroundColor: "#3cb371",
+              color: "#f8f9fa",
+              fontSize: "16px",
+              cursor: "pointer",
+              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            Retry Login with Google
+          </button>
+          <p style={{ marginTop: "10px" }}>{email}</p>
         </div>
       ) : (
-        <div>
-          {/* <h1>Login</h1> */}
-          <button onClick={login}>Login with Google</button>
+        <div style={{ textAlign: "center" }}>
+          <button
+            onClick={login}
+            style={{
+              padding: "15px 30px",
+              borderRadius: "5px",
+              border: "none",
+              backgroundColor: "#3cb371",
+              color: "#f8f9fa",
+              fontSize: "16px",
+              cursor: "pointer",
+              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            Login with Google
+          </button>
+          {error && (
+            <p style={{ color: "red", marginTop: "10px" }}>{error}</p>
+          )}
         </div>
       )}
     </div>
